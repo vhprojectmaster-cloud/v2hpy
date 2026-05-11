c6a328aa199b4476edff4b8060c36a3129b07585
vhprojectmaster-cloud <vhprojectmaster@gmail.com>
2026-05-11 19:57:21 +1000

    relay 10

---
+++ relay10.py
@@ -0,0 +1,13 @@
+from   import OutputDevice
+from time import sleep
+
+relay = OutputDevice(27, active_high=False, initial_value=False)
+
+while True:
+    print("Relay 2 ON")
+    relay.on()
+    sleep(10)
+
+    print("Relay 2 OFF")
+    relay.off()
+    sleep(10)
