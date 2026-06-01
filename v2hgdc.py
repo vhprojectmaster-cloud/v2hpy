50b878ac9f8fef6db2d7e72b531bdaa6a6c0b536
vhprojectmaster-cloud <vhprojectmaster@gmail.com>
2026-06-01 19:04:38 +1000

    error

--- v2hgdc.py
+++ v2hgdc.py
@@ -215,5 +215,5 @@ def main():
         print(f"Log saved to: {log_file}")
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
--- v2hs.py
+++ v2hs.py
@@ -693,5 +693,5 @@ def main():
         print("Simulation complete.")
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
